4857974680de81a8186f152a0b6915c912493250
connerCpowell <copowengineering@gmail.com>
2024-05-31 16:49:44 -0700

    Adding initial notebook

---
+++ Wine_forecasting.py
@@ -0,0 +1,60 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+ 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+ 
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# %%
+
+df = pd.read_csv('./winequality/winequality-red.csv')
+print(df.head())
+
+
+# %%
+df.info()
+
+# %%
+df.describe().T
+
+# %%
+#df.isnull().sum()
+
+for col in df.columns:
+  if df[col].isnull().sum() > 0:
+    df[col] = df[col].fillna(df[col].mean())
+ 
+df.isnull().sum().sum()
+
+# %%
+df.hist(bins=20, figsize=(10, 10))
+plt.show()
+
+
+# %%
